880c849c9457c6c0e32203fa922b735e7e362b74
MacTaylor <cslag@hotmail.com.tw>
2019-03-03 22:38:20 +0800

    trans send url

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"io/ioutil"
+	"net/http"
 
-func main(){
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
 	r := gin.Default()
-	r.GET("/proxy-api", func(c *gin.Context){
+	r.GET("/proxy-api", func(c *gin.Context) {
 		c.String(200, "Proxy is working.")
 	})
+
+	r.POST("/ptx-service", func(c *gin.Context) {
+
+		apiurl := c.PostForm("apiString")
+		req, err := http.NewRequest("GET", apiurl, nil)
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		c.String(200, string(body))
+	})
+
+	r.Run(":80")
 }
